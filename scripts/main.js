a9a26ee2a0f4e63f1150d06d71a4f7c80a12e2d4
rchtect <90619276+rchtect@users.noreply.github.com>
2022-01-19 16:30:43 +0100

    Fix for totalTodo and date-Btn

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     }
         date[i].addEventListener("click", function (event) {
           localStorage.setItem("date", currentDate);
-        });
+        })
         let a = date[i].innerText
         if (JSON.parse(localStorage.getItem(`todos${a}`)) != null) {
           let showAllTodo = document.createElement("span")
